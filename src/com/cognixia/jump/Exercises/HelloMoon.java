6ad00e6bc8138a0928c5bf65cc494bd674915c50
David Morales <davmorales@ucdavis.edu>
2020-06-11 14:11:35 -0400

    Adding moon prog

---
+++ src/com/cognixia/jump/Exercises/HelloMoon.java
@@ -0,0 +1,10 @@
+package com.cognixia.jump.Exercises;
+
+public class HelloMoon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello moon");
+	}
+
+}
